40f569b167e73a2d168ad8078b75f9cbce2a646d
Cameron <122345679+camerond-bbd@users.noreply.github.com>
2023-06-25 13:50:06 +0200

    Database connection and the player game history query (Needs testing)

--- api/api.js
+++ api/api.js
@@ -1,16 +1,36 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
+const mysql = require("mysql2");
 
-app.get("/names", (req, res, next) => {
-    res.json(
-        {
-            status: "success",
-            names: ["Daelin", "Juan-Roux", "Anne-Mien", "Cameron"]
-        }
-    );
-});
+// app.get("/names", (req, res, next) => {
+//     res.json(
+//         {
+//             status: "success",
+//             names: ["Daelin", "Juan-Roux", "Anne-Mien", "Cameron"]
+//         }
+//     );
+// });
 
 app.listen(8082, () => {
     console.log("Server running on port 8082");
 });
 
+app.get("/history", (req, res) => {
+    const playerEmail = req.query.email;
+    const query = `SELECT playerId FROM playerInfo WHERE email = ?`;
+    const validEMailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    if (!query.match(validEMailPattern)) {
+        return res.status(400).json({ err: "Please enter a valid email" });
+    } else {
+        mysql.query(query, [playerEmail], (err, rows, fields) => {
+            if (!err) {
+                res.status(200).send(rows);
+            } else {
+                console.log(err);
+                res.status(400).send(fields);
+            };
+        });
+    };
+});
+
+app.post("")
