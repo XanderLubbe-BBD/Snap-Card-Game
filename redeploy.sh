a6ae7a2cbeb224f2818eaaea8f21895295a92173
Daelin Campleman <daelin.campleman@bbd.co.za>
2023-06-26 19:51:33 +0200

    command to recreate all containers

---
+++ redeploy.sh
@@ -0,0 +1,26 @@
+sudo docker stop authserver;
+sudo docker rm authserver;
+sudo docker stop appserver;
+sudo docker rm appserver;
+sudo docker stop apiserver;
+sudo docker rm apiserver;
+sudo docker stop app;
+sudo docker rm app;
+
+git pull;
+
+sudo cd auth-server;
+sudo docker build -t auth .;
+sudo docker run -p 4001:4001 -network Snapynetwork -name authserver -tld auth;
+
+sudo cd ../server;
+sudo docker build -t server .;
+sudo docker run -network Snapynetwork -name appserver -tld server;
+
+sudo cd ../api;
+sudo docker build -t api .;
+sudo docker run -network Snapynetwork -name apiserver -tld api;
+
+sudo cd ../app;
+sudo docker build -t app .;
+sudo docker run -p 8080:8080 -network Snapynetwork -name app -tld app;
