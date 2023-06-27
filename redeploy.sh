3c56644de7aa934c401f40f5e659f6e69eaa4170
Daelin Campleman <127960675+Daelin-Campleman@users.noreply.github.com>
2023-06-27 12:51:43 +0200

    Update redeploy.sh

--- redeploy.sh
+++ redeploy.sh
@@ -10,21 +10,21 @@ sudo docker rm appserver;
 git pull;
 
 cd auth-server;
-npm install;
+
 sudo docker build -t auth .;
 sudo docker run -d -p 4001:4001 --network Snapynetwork --name authserver -tld auth;
 
 cd ../server;
-npm install;
+
 sudo docker build -t server .;
 sudo docker run -d -p 8081:8081 --network Snapynetwork --name serverurl -tld server;
 
 cd ../api;
-npm install;
+
 sudo docker build -t api .;
 sudo docker run -d --network Snapynetwork --name apiserver -tld api;
 
 cd ../app;
-npm install;
+
 sudo docker build -t app .;
 sudo docker run -d -p 8080:8080 --network Snapynetwork --name appserver -tld app;
