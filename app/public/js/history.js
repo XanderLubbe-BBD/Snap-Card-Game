00b3a02ea399370f95feef3044cce760e09808c4
Daelin Campleman <daelin.campleman@bbd.co.za>
2023-06-27 17:45:01 +0200

    Rewritten history

--- app/public/js/history.js
+++ app/public/js/history.js
@@ -1,9 +1,43 @@
 window.onload = function (event) {
-  setTimeout(() => {
+  const ws = new WebSocket(`wss://d2lgqlnck8vz6t.cloudfront.net/socket`);
+  ws.addEventListener("open", () => {
+    console.log("Connected to server");
+
     const msg = {
       type: "history",
       token: sessionStorage.getItem("token")
     }
     sendMessage(msg);
-  }, 2000);
+  });
 };
+
+ws.addEventListener('message', function (event) {
+  let msg = JSON.parse(event.data);
+
+  switch (msg.type) {
+    case "history":
+      console.log(msg.history);
+      createHistoryTable(msg.history);
+      break;
+  }
+})
+
+function createHistoryTable(history) {
+  let place = document.getElementById("historyTable");
+
+  place.innerHTML = "";
+
+  let tr = document.createElement("tr");
+  let td = document.createElement("td");
+  let ul = document.createElement("ul");
+  ul.classList.add("name-list");
+  for (let i = 0; i < history.players.length; i++) {
+    let li = document.createElement("li");
+    li.textContent = history.players[i];
+    ul.appendChild(li);
+  }
+
+  td.appendChild(ul);
+  tr.appendChild(td);
+  place.appendChild(tr);
+}
