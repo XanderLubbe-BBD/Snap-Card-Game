63e0082fbd84a4aec3ecaeab45b126ab2ef5c3e7
Daelin Campleman <127960675+Daelin-Campleman@users.noreply.github.com>
2023-06-27 12:54:26 +0200

    Update redeploy.sh

--- redeploy.sh
+++ redeploy.sh
@@ -12,19 +12,19 @@ git pull;
 cd auth-server;
 
 sudo docker build -t auth .;
-sudo docker run -d -p 4001:4001 --network Snapynetwork --name authserver -tld auth;
+sudo docker run -d -p 4001:4001 --network Snapynetwork --name authserver --restart unless-stopped -tld auth;
 
 cd ../server;
 
 sudo docker build -t server .;
-sudo docker run -d -p 8081:8081 --network Snapynetwork --name serverurl -tld server;
+sudo docker run -d -p 8081:8081 --network Snapynetwork --name serverurl --restart unless-stopped -tld server;
 
 cd ../api;
 
 sudo docker build -t api .;
-sudo docker run -d --network Snapynetwork --name apiserver -tld api;
+sudo docker run -d --network Snapynetwork --name apiserver --restart unless-stopped -tld api;
 
 cd ../app;
 
 sudo docker build -t app .;
-sudo docker run -d -p 8080:8080 --network Snapynetwork --name appserver -tld app;
+sudo docker run -d -p 8080:8080 --network Snapynetwork --name appserver --restart unless-stopped -tld app;
