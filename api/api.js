e6fba86afdacc829e46f4879582643b042a5ca2b
Daelin Campleman <daelin.campleman@bbd.co.za>
2023-06-27 18:37:28 +0200

    Update api.js

--- api/api.js
+++ api/api.js
@@ -11,59 +11,45 @@ app.listen(8082, () => {
 app.get("/history/:token", verifyEmail, (req, res, next) => {
     const playerEmail = res.locals.email;
     try {
-        const query = `
-        SELECT
-  Games.game_id,
-  Players.username,
-  Winner.username AS winner_username
-FROM
-  Game_Players
-  INNER JOIN Players ON Players.player_id = Game_Players.player_id
-  INNER JOIN Games ON Games.game_id = Game_Players.game_id
-  INNER JOIN Players AS Winner ON Winner.player_id = Games.winner_id
-WHERE
-  Game_Players.game_id IN (
-    SELECT
-      game_id
-    FROM
-      Game_Players
-    WHERE
-      player_id = (
-        SELECT
-          player_id
-        FROM
-          Players
-        WHERE
-          email = ?
-      )
-  );
-        `;
+        const query = `SELECT Players.username, Games.winner_id, Winners.username AS winner_username
+      FROM Game_Players
+      INNER JOIN Players ON Players.player_id = Game_Players.player_id
+      INNER JOIN Games ON Games.game_id = Game_Players.game_id
+      INNER JOIN Players AS Winners ON Winners.player_id = Games.winner_id
+      WHERE Game_Players.game_id IN (
+        SELECT game_id
+        FROM Game_Players
+        WHERE player_id = (
+          SELECT player_id
+          FROM Players
+          WHERE email = ?
+        )
+      );`;
 
 
 
-        pool.query(query, [playerEmail], (err, rows) => {
+        pool.query(query, [playerEmail], (err, rows, fields) => {
             if (!err) {
-                const games = [];
-                const gameMap = new Map();
-
-                rows.forEach(row => {
-                    const { game_id, username, winner_id } = row;
-                    if (!gameMap.has(game_id)) {
-                        const game = {
-                            game_id,
-                            players: [username],
-                            winner_id
-                        };
-                        games.push(game);
-                        gameMap.set(game_id, game);
+                let obj = [];
+                let gameIds = [];
+                rows.forEach((row) => {
+                    if (gameIds.includes(row.game_id)) {
+                        obj.forEach((item) => {
+                            if (item.game_id == row.game_id) {
+                                item.players.push(row.username);
+                            }
+                        });
                     } else {
-                        gameMap.get(game_id).players.push(username);
+                        let newObj = {
+                            game_id: row.game_id,
+                            players: [row.username],
+                            winner: row.winner_username,
+                        };
+                        obj.push(newObj);
+                        gameIds.push(row.game_id);
                     }
                 });
-
-
-
-                res.status(200).json(games);
+                res.status(200).send(obj);
             } else {
                 console.log(err);
                 res.status(400).send(fields);
