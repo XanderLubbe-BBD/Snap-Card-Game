5bfba3baa15f27ce28f5379613208b89c65384d7
Daelin Campleman <127960675+Daelin-Campleman@users.noreply.github.com>
2023-06-27 11:34:00 +0200

    Update app.js

--- app/app.js
+++ app/app.js
@@ -70,7 +70,7 @@ async function verifyUser(req,res,next){
 
 const getAuth = async (url, header) => {
     try {
-      const response = await fetch(`${process.env.AUTH_URL}/${url}`, {
+      const response = await fetch(`https://d2lgqlnck8vz6t.cloudfront.net/${url}`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
