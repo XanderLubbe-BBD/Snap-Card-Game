cc4f47246e72b064dd0fc55e9da2c0419ec4d462
Daelin Campleman <daelin.campleman@bbd.co.za>
2023-06-27 18:03:51 +0200

    Update api.js

--- api/api.js
+++ api/api.js
@@ -8,51 +8,56 @@ app.listen(8082, () => {
     console.log("Server running on port 8082");
 });
 
-app.get("/history/:token",verifyEmail, (req, res, next) => {
+app.get("/history/:token", verifyEmail, (req, res, next) => {
     const playerEmail = res.locals.email;
-    try{
-    const query = `SELECT Players.username, Games.winner_id
-    FROM Game_Players
-    INNER JOIN Players ON Players.player_id = Game_Players.player_id
-    INNER JOIN Games ON Games.game_id = Game_Players.game_id
-    WHERE Game_Players.game_id IN (
-      SELECT game_id
-      FROM Game_Players
-      WHERE player_id = (
-        SELECT player_id
-        FROM Players
-        WHERE email = ?
-      )
-    );`;
-    pool.query(query, [playerEmail], (err, rows, fields) => {
-        if (!err) {
-            let obj = [];
-            let gameIds = [];
-            rows.forEach(row => {
-                if(gameIds.includes(row.game_id)){
-                    obj.forEach(item => {
-                        if(item.game_id == row.game_id){
-                            item.players.push(row.username);
-                        }
-                    });
-                } else {
-                    let newObj = {
-                        game_id: row.game_id,
-                        players: [row.username]
+    try {
+        const query = `
+        SELECT Players.username, Games.game_id, Games.winner_id
+        FROM Game_Players
+        INNER JOIN Players ON Players.player_id = Game_Players.player_id
+        INNER JOIN Games ON Games.game_id = Game_Players.game_id
+        WHERE Game_Players.game_id IN (
+          SELECT game_id
+          FROM Game_Players
+          WHERE player_id = (
+            SELECT player_id
+            FROM Players
+            WHERE email = ?
+          )
+        );`;
+
+
+
+        pool.query(query, [playerEmail], (err, rows) => {
+            if (!err) {
+                const games = [];
+                const gameMap = new Map();
+
+                rows.forEach(row => {
+                    const { game_id, username, winner_id } = row;
+                    if (!gameMap.has(game_id)) {
+                        const game = {
+                            game_id,
+                            players: [username],
+                            winner_id
+                        };
+                        games.push(game);
+                        gameMap.set(game_id, game);
+                    } else {
+                        gameMap.get(game_id).players.push(username);
                     }
-                    obj.push(newObj);
-                    gameIds.push(row.game_id);
-                }
-            })
-            res.status(200).send(obj);
-        } else {
-            console.log(err);
-            res.status(400).send(fields);
-        };
-    });
-    }
-    catch (err) {
-        next(err)
+                });
+
+
+
+                res.status(200).json(games);
+            } else {
+                console.log(err);
+                res.status(400).send(fields);
+            }
+        });
+    } catch (err) {
+        next(err);
     }
 });
 
@@ -60,13 +65,13 @@ app.get("/info/:token", verifyEmail, (req, res) => {
     console.log("Getting info");
     const playerEmail = res.locals.email;
     console.log(`Email: ${playerEmail}`);
-    try{
+    try {
         const query = `SELECT username, email FROM Players WHERE email = ?`;
         console.log(`Running SQL Query: ${query.replace('?', `'${playerEmail}'`)}`);
         pool.query(query, [playerEmail], (err, rows, fields) => {
             if (!err) {
                 console.log(rows);
-                const response = {username: rows[0].username, email: rows[0].email, valid: true}
+                const response = { username: rows[0].username, email: rows[0].email, valid: true }
                 res.status(200).send(response);
             } else {
                 console.log(err);
@@ -74,20 +79,20 @@ app.get("/info/:token", verifyEmail, (req, res) => {
             };
         });
     }
-    catch{
-        
+    catch {
+
     }
 });
 
 app.get("/register/:token", verifyAndRetrieveUser, (req, res) => {
     const playerEmail = res.locals.email;
     const userName = res.locals.firstName + ' ' + res.locals.lastName;
-    try{
+    try {
         const query = `INSERT INTO Players(username, email) VALUES (?, ?)`;
         pool.query(query, [userName, playerEmail], (err, rows, fields) => {
             if (!err) {
                 console.log(rows);
-                const response = {email: rows.email}
+                const response = { email: rows.email }
                 res.status(200).send(response);
             } else {
                 console.log(err);
@@ -95,11 +100,11 @@ app.get("/register/:token", verifyAndRetrieveUser, (req, res) => {
             };
         });
     }
-    catch(error){
+    catch (error) {
     }
 })
 
-async function verifyAndRetrieveUser(req, res, next){
+async function verifyAndRetrieveUser(req, res, next) {
     const token = req.params.token;
     const result = await getAuth("user", token);
     res.locals.email = result.email;
@@ -109,7 +114,7 @@ async function verifyAndRetrieveUser(req, res, next){
     next();
 }
 
-async function verifyEmail(req, res, next){
+async function verifyEmail(req, res, next) {
     const token = req.params.token;
     const result = await getAuth("email", token);
     res.locals.email = result.email;
@@ -118,19 +123,20 @@ async function verifyEmail(req, res, next){
 
 const getAuth = async (url, header) => {
     try {
-      const response = await fetch(`${process.env.AUTH_URL}/${url}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'x-access-token' : header,
-        },
-      });
-      const json = await response.json();
-  
-      return json;
+        const response = await fetch(`${process.env.AUTH_URL}/${url}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': header,
+            },
+        });
+        const json = await response.json();
+
+        return json;
     } catch (error) {
-      console.log(error);
-    }};
+        console.log(error);
+    }
+};
 
 
 app.post('/gameResults', (req, res) => {
@@ -143,38 +149,38 @@ app.post('/gameResults', (req, res) => {
         res.status(400).json({ success: false, error: 'At least 2 players are required.' });
         return;
     }
-  
+
     const gameQuery = 'INSERT INTO Games (winner_id) VALUES ((SELECT player_id FROM Players WHERE email = ?))';
     pool.query(gameQuery, [winner], (error, results) => {
         if (error) {
             console.error('Error inserting game:', error);
-            res.status(500).send({success: false});
+            res.status(500).send({ success: false });
             return;
         }
-  
+
         const gameId = results.insertId;
-    
+
         let placeholders = '';
         const values = [];
-    
+
         for (let i = 0; i < players.length; i++) {
             placeholders += '(?, (SELECT player_id FROM Players WHERE email = ?)), ';
             values.push(gameId, players[i]);
         }
-    
+
         placeholders = placeholders.slice(0, -2);
-    
+
         const gamePlayersQuery =
             `INSERT INTO Game_Players (game_id, player_id) VALUES ${placeholders}`;
-        
+
         pool.query(gamePlayersQuery, values, (error) => {
             if (error) {
-            console.error('Error inserting game players:', error);
-            res.status(500).send({success: false});
-            return;
+                console.error('Error inserting game players:', error);
+                res.status(500).send({ success: false });
+                return;
             }
-    
-                res.status(200).send({success: true});
+
+            res.status(200).send({ success: true });
         });
     });
 });
