05dd0f8673598e31cc22c83924c9d57444e6c4c6
Daelin Campleman <127960675+Daelin-Campleman@users.noreply.github.com>
2023-06-27 11:31:03 +0200

    Update login.js

--- app/public/js/login.js
+++ app/public/js/login.js
@@ -1,7 +1,7 @@
 
   const postAuth = async (url, body) => {
     try {
-      const response = await fetch(`http://13.246.63.33:4001/${url}`, {
+      const response = await fetch(`https://d2lgqlnck8vz6t.cloudfront.net/${url}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
