541deaa14c7fe237917f4630b00df94c7a41c4fd
Daelin Campleman <127960675+Daelin-Campleman@users.noreply.github.com>
2023-06-27 11:31:23 +0200

    Update signup.js

--- app/public/js/signup.js
+++ app/public/js/signup.js
@@ -1,6 +1,6 @@
 const postAuth = async (url, body) => {
     try {
-      const response = await fetch(`${process.env.AUTH_URL}/${url}`, {
+      const response = await fetch(`https://d2lgqlnck8vz6t.cloudfront.net/${url}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
